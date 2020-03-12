0f2e424733ccad9e620a62624214b94ad28ca52e
Draco Zhou <draco.coder@gmail.com>
2020-03-12 11:16:21 +0800

    [add]: Nuxt-Express-Template

---
+++ Nuxt-Express-Template/middleware/user-agent.js
@@ -0,0 +1,4 @@
+export default function (context) {
+  // Add the userAgent property in the context (available in `data` and `fetch`)
+  context.userAgent = context.isServer ? context.req.headers['user-agent'] : navigator.userAgent
+}
